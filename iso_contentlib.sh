17b37d3a16366bcd47a65addb06f8cdb6fc3209e
michaelcade <michael.cade@outlook.com>
2025-03-07 08:11:15 +0000

    Added some Linux Desktop ISOs, I want to try

--- iso_contentlib.sh
+++ iso_contentlib.sh
@@ -1,6 +1,6 @@
-#A script to pull down ISOs to your content library, change to your Content Library name below, chmod it and run the script. You will need govc and you will need to export connection details for your vSphere environment
 #!/bin/bash
 
+#A script to pull down ISOs to your content library, change to your Content Library name below, chmod it and run the script. You will need govc and you will need to export connection details for your vSphere environment
 # Set your vSphere Content Library name
 CONTENT_LIBRARY="vZilla-Content-Library"
 
@@ -27,6 +27,20 @@ ISO_URLS=(
     "https://software-static.download.prss.microsoft.com/dbazure/988969d5-f34g-4e03-ac9d-1f9786c66749/17763.3650.221105-1748.rs5_release_svc_refresh_SERVER_EVAL_x64FRE_en-us.iso"
     "https://software-static.download.prss.microsoft.com/dbazure/888969d5-f34g-4e03-ac9d-1f9786c66749/22631.2428.231001-0608.23H2_NI_RELEASE_SVC_REFRESH_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
     "https://software-static.download.prss.microsoft.com/dbazure/988969d5-f34g-4e03-ac9d-1f9786c66750/19045.2006.220908-0225.22h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
+    "https://cdimage.debian.org/debian-cd/current-live/amd64/iso-hybrid/debian-live-12.9.0-amd64-kde.iso"
+    "https://archlinux.uk.mirror.allworldit.com/archlinux/iso/2025.03.01/archlinux-2025.03.01-x86_64.iso"
+    "https://channels.nixos.org/nixos-24.11/latest-nixos-plasma6-x86_64-linux.iso"
+    "https://channels.nixos.org/nixos-24.11/latest-nixos-gnome-x86_64-linux.iso"
+    "https://repo.almalinux.org/almalinux/9.5/isos/x86_64/AlmaLinux-9.5-x86_64-dvd.iso"
+    "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-virt-3.21.3-x86_64.iso"
+    "https://iso.artixlinux.org/iso/artix-plasma-dinit-20240823-x86_64.iso"
+    "https://cdimage.kali.org/kali-2024.4/kali-linux-2024.4-installer-amd64.iso"
+    "https://deb.parrot.sh/parrot/iso/6.3.2/Parrot-security-6.3.2_amd64.iso"
+    "https://cdimage.ubuntu.com/kubuntu/releases/24.10/release/kubuntu-24.10-desktop-amd64.iso"
+    "https://mirrors.cicku.me/linuxmint/iso/stable/22.1/linuxmint-22.1-cinnamon-64bit.iso"
+    "https://cdimage.ubuntu.com/lubuntu/releases/noble/release/lubuntu-24.04.2-desktop-amd64.iso"
+    "https://downloads.getsol.us/isos/2025-01-26/Solus-Budgie-Release-2025-01-26.iso"
+    "https://distro.ibiblio.org/tinycorelinux/15.x/x86/release/CorePlus-current.iso"
 )
 
 # Maximum length for filenames
